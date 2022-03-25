4ec6c2b9f2923706db764c58d15df1d90d1e0829
Brian Teets <bwt17@msn.com>
2022-03-25 16:37:40 -0600

    initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,11 @@
+package com.skilldistillery.foodtruck.app;
+
+public class FoodTruckApp {
+	// inside of FoodTruckApp class will contain array of up to five FoodTruck objects
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private static int nextTruckId; // every food truck will have a unique numeric ID
+	private static String name; // every food truck will have a unique name
+
+}
